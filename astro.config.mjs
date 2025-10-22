de5ff8048aabebdfc02f91ee2f2026bc570892f6
jemmdevs <jemmdev@gmail.com>
2025-10-22 20:04:13 +0200

    deploy changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,5 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://josendev.com',
-  build: {
-    format: 'file'
-  }
+  site: 'https://josendev.com'
 });
